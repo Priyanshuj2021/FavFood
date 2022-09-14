2e3f4df746bd2aefa7439057b7cb9c88a357ccf0
Priyanshuj2021 <111872911+Priyanshuj2021@users.noreply.github.com>
2022-09-14 17:16:00 +0530

    Initial commit

---
+++ src/components/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li> Dosa</li>
+      <li> Panner </li>
+      <li> Pav Bhaji </li>
+    </ul>
+  );
+}
+export default List;
